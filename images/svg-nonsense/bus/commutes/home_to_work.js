9be85287d6cd93370fbccae194a71de57307cfbe
Ben Nesson <bnesson@tableau.com>
2017-07-31 18:03:36 -0700

    lambdaify walkStep and busStep in home_to_work and work_to_home

--- images/svg-nonsense/bus/commutes/home_to_work.js
+++ images/svg-nonsense/bus/commutes/home_to_work.js
@@ -1,12 +1,7 @@
 /// <reference path="../OBA_data.js" />
 
-function walk(min) {
-    return { w: min };
-}
-
-function busStep(route, start, end) {
-    return { r: route, s: start, e: end };
-}
+var walkStep = min => ({ w: min });
+var busStep = (route, start, end) => ({ r: route, s: start, e: end });
 
 function defineJourneys() {
     var d = defineIdentifiers();
@@ -15,61 +10,61 @@ function defineJourneys() {
             name: '45 -> 5',
             steps: [
                 busStep(d.route.Number45, d.stop.OnAurora.At85th.WB, d.stop.OnGreenwood.At85th.WB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.Number5, d.stop.OnGreenwood.At85th.SB, d.stop.OnAurora.At38th.SEB),
-                walk(15)
+                walkStep(15)
             ]
         },
         {
             name: '45 -> 28X',
             steps: [
                 busStep(d.route.Number45, d.stop.OnAurora.At85th.WB, d.stop.On8th.At85th.WB),
-                walk(3),
+                walkStep(3),
                 busStep(d.route.Number28X, d.stop.On8th.At85th.SB, d.stop.OnAurora.At38th.SEB),
-                walk(15)
+                walkStep(15)
             ]
         },
         {
             name: 'E-Line -> walk',
             steps: [
                 busStep(d.route.ELine, d.stop.OnAurora.At85th.SB, d.stop.OnAurora.At46th.SB),
-                walk(30)
+                walkStep(30)
             ]
         },
         {
             name: 'E-Line -> 62 SB',
             steps: [
                 busStep(d.route.ELine, d.stop.OnAurora.At85th.SB, d.stop.OnAurora.At46th.SB),
-                walk(6),
+                walkStep(6),
                 busStep(d.route.Number62, d.stop.OnStone.At45th.SB, d.stop.OnStone.At35th.WB),
-                walk(8)
+                walkStep(8)
             ]
         },
         {
             name: 'E-Line -> 28X',
             steps: [
                 busStep(d.route.ELine, d.stop.OnAurora.At85th.SB, d.stop.OnAurora.AtGaler.SB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.Number28X, d.stop.OnAurora.AtGaler.NB, d.stop.OnAurora.At38th.NWB),
-                walk(16)
+                walkStep(16)
             ]
         },
         {
             name: 'E-Line -> 62 NB',
             steps: [
                 busStep(d.route.ELine, d.stop.OnAurora.At85th.SB, d.stop.OnAurora.AtGaler.SB),
-                walk(3),
+                walkStep(3),
                 busStep(d.route.Number62, d.stop.OnDexter.AtGaler.NB, d.stop.OnStone.At35th.EB),
-                walk(7)
+                walkStep(7)
             ]
         },
         {
             name: '45 -> 62',
             steps: [
                 busStep(d.route.Number45, d.stop.OnStone.At85th.EB, d.stop.OnRavenna.AtWoodlawn.SEB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.Number62, d.stop.OnRavenna.AtWoodlawn.SWB, d.stop.OnStone.At35th.WB),
-                walk(8)
+                walkStep(8)
             ]
         }
     ];
--- images/svg-nonsense/bus/commutes/work_to_home.js
+++ images/svg-nonsense/bus/commutes/work_to_home.js
@@ -1,12 +1,7 @@
 /// <reference path="../OBA_data.js" />
 
-function walk(min) {
-    return { w: min };
-}
-
-function busStep(route, start, end) {
-    return { r: route, s: start, e: end };
-}
+var walkStep = min => ({ w: min });
+var busStep = (route, start, end) => ({ r: route, s: start, e: end });
 
 function defineJourneys() {
     var d = defineIdentifiers();
@@ -14,72 +9,72 @@ function defineJourneys() {
         {
             name: '32 -> 45',
             steps: [
-                walk(3),
+                walkStep(3),
                 busStep(d.route.Number32, d.stop.OnWoodlawn.At35th.EB, d.stop.OnUniversity.AtCampusParkway.EB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.Number45, d.stop.OnUniversity.At41st.NB, d.stop.OnStone.At85th.WB)
             ]
         },
         {
             name: '31 -> 45',
             steps: [
-                walk(3),
+                walkStep(3),
                 busStep(d.route.Number31, d.stop.OnWoodlawn.At35th.EB, d.stop.OnUniversity.AtCampusParkway.EB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.Number45, d.stop.OnUniversity.At41st.NB, d.stop.OnStone.At85th.WB)
             ]
         },
         {
             name: '62 SB -> E-Line',
             steps: [
-                walk(7),
+                walkStep(7),
                 busStep(d.route.Number62, d.stop.OnStone.At35th.WB, d.stop.OnDexter.AtGaler.SB),
-                walk(4),
+                walkStep(4),
                 busStep(d.route.ELine, d.stop.OnAurora.AtGaler.NB, d.stop.OnAurora.At85th.NB)
             ]
         },
         {
             name: '62 -> 45',
             steps: [
-                walk(8),
+                walkStep(8),
                 busStep(d.route.Number62, d.stop.OnStone.At35th.EB, d.stop.OnRavenna.AtWoodlawn.SEB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.Number45, d.stop.OnRavenna.AtWoodlawn.NWB, d.stop.OnStone.At85th.WB)
             ]
         },
         {
             name: '62 NB -> E-Line',
             steps: [
-                walk(8),
+                walkStep(8),
                 busStep(d.route.Number62, d.stop.OnStone.At35th.EB, d.stop.OnStone.At45th.EB),
-                walk(7),
+                walkStep(7),
                 busStep(d.route.ELine, d.stop.OnAurora.At46th.NB, d.stop.OnAurora.At85th.NB)
             ]
         },
         {
             name: '28X -> 45',
             steps: [
-                walk(15),
+                walkStep(15),
                 busStep(d.route.Number28X, d.stop.OnAurora.At38th.NWB, d.stop.On8th.At85th.NB),
-                walk(3),
+                walkStep(3),
                 busStep(d.route.Number45, d.stop.On8th.At85th.EB, d.stop.OnAurora.At85th.EB)
             ]
         },
         {
             name: '5 -> 45',
             steps: [
-                walk(15),
+                walkStep(15),
                 busStep(d.route.Number5, d.stop.OnAurora.At38th.NWB, d.stop.OnGreenwood.At85th.NB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.Number45, d.stop.OnGreenwood.At85th.EB, d.stop.OnAurora.At85th.EB)
             ]
         },
         {
             name: '28X -> E-Line',
             steps: [
-                walk(16),
+                walkStep(16),
                 busStep(d.route.Number28X, d.stop.OnAurora.At38th.SEB, d.stop.OnAurora.AtGaler.SB),
-                walk(2),
+                walkStep(2),
                 busStep(d.route.ELine, d.stop.OnAurora.AtGaler.NB, d.stop.OnAurora.At85th.NB)
             ]
         }
